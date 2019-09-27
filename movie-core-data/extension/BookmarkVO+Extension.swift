3c44fc833c42fd7d0f02d91660bab78ef5568cb0
ShunLH <shunleihmu.shunlh@gmail.com>
2019-09-28 00:25:59 +0630

    commit

---
+++ movie-core-data/extension/BookmarkVO+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  BookMarkVO+Extension.swift
+//  movie-core-data
+//
+//  Created by AcePlus Admin on 9/27/19.
+//  Copyright © 2019 padc. All rights reserved.
+//
+
+import Foundation
---
+++ movie-core-data/views/cells/BookmarksListCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BookmarksListCollectionViewCell.swift
+//  movie-core-data
+//
+//  Created by AcePlus Admin on 9/26/19.
+//  Copyright © 2019 padc. All rights reserved.
+//
+
+import UIKit
+
+class BookmarksListCollectionViewCell: UICollectionViewCell {
+    
+}
